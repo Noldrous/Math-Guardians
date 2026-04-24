1b3d8eefce03236fb54511b9dfb8207e183d1422
justincapia14-art <justincapia14@gmail.com>
2026-04-24 20:28:15 +0800

    third commit

---
+++ tilemap1.py
@@ -0,0 +1,63 @@
+import pygame
+
+class TileMap:
+    def __init__(self):
+        self.boxes = []
+        
+        for i in range(5):
+            self.boxes.append(pygame.Rect(20, 130 + (i * 90), 50, 50))
+        
+        self.placed_towers = [] 
+
+        self.selected_box_index = -1 
+        self.holding_color = None     
+        self.show_menu = False        
+
+        # TINANGGAL ANG YELLOW (255,255,0)
+        self.colors = [(255,0,0), (0,255,0), (0,0,255)] 
+
+    def handle_click(self, pos):
+        if self.holding_color != None:
+            box = self.boxes[self.selected_box_index]
+            if pos[1] >= box.top and pos[1] <= box.bottom:
+                if pos[0] > box.right and pos[0] <= 500:
+                    self.placed_towers.append([pos[0]-20, pos[1]-20, self.holding_color])
+            
+            self.holding_color = None
+            return
+
+        if self.show_menu == True:
+            box = self.boxes[self.selected_box_index]
+            # PINALITAN ANG range(4) NG len(self.colors)
+            for i in range(len(self.colors)):
+                menu_btn = pygame.Rect(box.right + 10 + (i * 45), box.top + 15, 35, 35)
+                if menu_btn.collidepoint(pos):
+                    self.holding_color = self.colors[i]
+            
+            self.show_menu = False
+            return
+
+        for i in range(len(self.boxes)):
+            if self.boxes[i].collidepoint(pos):
+                self.selected_box_index = i
+                self.show_menu = True
+
+    def draw(self, screen):
+        pygame.draw.line(screen, (255, 0, 0), (500, 0), (500, 720), 2)
+
+        for box in self.boxes:
+            pygame.draw.rect(screen, (200, 200, 200), box) # Gray box
+            pygame.draw.rect(screen, (0, 0, 0), box, 2)    # Black border
+
+        if self.show_menu == True:
+            box = self.boxes[self.selected_box_index]
+            # PINALITAN ANG range(4) NG len(self.colors)
+            for i in range(len(self.colors)): 
+                pygame.draw.rect(screen, self.colors[i], (box.right + 10 + (i*45), box.top + 15, 35, 35))
+
+        for tower in self.placed_towers:
+            pygame.draw.rect(screen, tower[2], (tower[0], tower[1], 40, 40))
+
+        if self.holding_color != None:
+            m_pos = pygame.mouse.get_pos()
+            pygame.draw.rect(screen, self.holding_color, (m_pos[0]-20, m_pos[1]-20, 40, 40), 2)
